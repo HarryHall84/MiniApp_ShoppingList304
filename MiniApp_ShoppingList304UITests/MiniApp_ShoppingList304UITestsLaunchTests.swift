ec7fa13e3fd1d6aa0b8db5e621878573fdce853d
Harrison Hall <harrison.hall23@d155.org>
2021-10-19 07:25:45 -0500

    Initial Commit

---
+++ MiniApp_ShoppingList304UITests/MiniApp_ShoppingList304UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MiniApp_ShoppingList304UITestsLaunchTests.swift
+//  MiniApp_ShoppingList304UITests
+//
+//  Created by Harrison Hall on 10/19/21.
+//
+
+import XCTest
+
+class MiniApp_ShoppingList304UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
